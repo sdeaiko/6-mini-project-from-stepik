382384d414d453bcc5133fbec82ac1181a2a5cb3
Sayan Nurmuhambet <s.deaiko@gmail.com>
2022-09-03 23:13:25 +0600

    strong pass edited

--- strong_password.py
+++ strong_password.py
@@ -1,3 +1,4 @@
+from operator import length_hint
 from random import *
 
 digits = '0123456789'
@@ -7,32 +8,46 @@ punctuation = '!#$%&*+-=?@^_'
 
 chars = ''
 isFinish = 'Д'
+arr = list()
 
-while isFinish == 'Д':
+while True:
     
     print("Будете ли вы генерировать код с цифрами?")
-    ans = input("Да = Д, Нет = н")
+    ans = input("Да = Д, Нет = н: ")
     if ans == 'Д':
         chars += digits
         
     print("Будете ли вы генерировать код со строчными буквами?")
-    ans = input("Да = Д, Нет = Н")
+    ans = input("Да = Д, Нет = Н: ")
     if ans == 'Д':
         chars += lowercase_letters
     
     print("Будете ли вы генерировать код с прописными буквами?")
-    ans = input("Да = Д, Нет = Н")
+    ans = input("Да = Д, Нет = Н: ")
     if ans == 'Д':
         chars += uppercase_letters
         
     print("Будете ли вы генерировать код со специальными символами?")
-    ans = input("Да = Д, Нет = Н")
+    ans = input("Да = Д, Нет = Н: ")
     if ans == 'Д':
         chars += punctuation
         
     print('Исключать ли неоднозначные символы il1Lo0?')
-    ans = input("Да = Д, Нет = Н")
+    ans = input("Да = Д, Нет = Н: ")
     if ans == 'Д':
         arr = [i for i in chars if i not in "il1Lo0"]
+    break
+
+def generate_password(length, arr):
+    passwords = ''
+    for i in range(length):
+        passwords += choice(arr)
+    return passwords
+
+length_password = int(input('Введите длину пароля: '))
+amount_password = int(input('Введите количество паролей: '))
+
+for i in range(amount_password):
+    print(generate_password(length_password, arr))
+
     
-    print(arr)
